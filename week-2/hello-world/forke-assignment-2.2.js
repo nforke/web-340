dea2de4b48ca6950673941c4873001b547199e24
Nicole Forke <n.forke@gmail.com>
2020-02-28 22:43:33 -0600

    Adding fir commit for week-2

---
+++ week-2/hello-world/forke-assignment-2.2.js
@@ -0,0 +1,29 @@
+/*
+===========================================
+;Title: Assignment 2.2
+;Author: Nicole Forke
+;Date: 28 February 2020
+;Modified By: Nicole Forke
+;Description: Hello World with Express
+===========================================
+*/
+
+//create express module
+var express = require("express");
+
+var http = require("http");
+
+//call the express function to start a new application
+var app = express();
+
+app.use(function(req, res) {
+
+  console.log("In comes a request to: %s", req.url);
+
+  res.end("Hello World\n");
+});
+
+//create a new server display message on port 8080
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port %s", 8080);
+});
